fd934d7a2930b08f60495bd7a243334f835ea1b9
davshoward <hi@davshoward.com>
2019-05-28 15:52:08 +0100

    Fix some caps

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
-export { default as Nav } from './nav';
+export { default as Nav } from './Nav';
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,2 +1,2 @@
-export { default as Layout } from './layout';
+export { default as Layout } from './Layout';
 export { default as SEO } from './SEO';
